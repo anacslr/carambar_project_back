62115fc35541d78136173823432431b75539b3fe
anacslr <anais.cieslar@gmail.com>
2025-07-18 08:39:03 +0200

    updated seed

--- seed.js
+++ seed.js
@@ -8,7 +8,6 @@ const jokesSeed = async () => {
             { question: "Que dit un oignon quand il se cogne ?", answer: "Aïe" },
             { question: "Quel est l'animal le plus heureux ?", answer: "Le hibou, parce que sa femme est chouette." },
             { question: "Pourquoi le football c'est rigolo ?", answer: "Parce que Thierry en rit" },
-            { question: "Quel est le sport le plus fruité ?", answer: "La boxe, parce que tu te prends des pêches dans la poire et tu tombes dans les pommes." },
             { question: "Que se fait un Schtroumpf quand il tombe ?", answer: "Un Bleu" },
             { question: "Quel est le comble pour un marin ?", answer: "Avoir le nez qui coule" },
             { question: "Qu'est ce que les enfants usent le plus à l'école ?", answer: "Le professeur" },
